e93191cf76f6bf9ac217b5ae45b7882242b9f36f
Gerges Malak <gmf20003@gmail.com>
2026-04-08 18:35:49 +0200

    Add project files.

---
+++ G-Net-34-LINQ02/Helper/StringCaseInsensitiveComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_34_LINQ02.Helper
+{
+    internal class StringCaseInsensitiveComparer : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            return string.Compare(x, y, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ G-Net-34-LINQ02/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_34_LINQ02.Models
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+        public string Major { get; set; }
+    }
+}
